9f6140627288b6129c87a90d7db8b6c1221937b5
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-05-29 16:00:54 +0200

    Completed Exercise 2

---
+++ Brayn Meyer - JavaScript - Exercise 2.js
@@ -0,0 +1,12 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 5
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 15
+// TODO: Convert number1 to a string and store it in a new variable
+let num1str = number1.toString()
+// TODO: Add number1 and number2 and store the result in a variable called sum
+let sum = number1 + number2
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+let product = number1 * number2
+// TODO: Print all results to the console
+console.log("The results are " + sum + ", " + product + ".")
